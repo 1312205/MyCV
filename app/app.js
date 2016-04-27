059aa4fd8cfcd2b5db610114b13049dbe287441b
Hiện Nguyễn <Hin Nguyn>
2016-04-27 20:02:13 +0700

    Tao file html, load cac file .json chua du lieu thong tin

--- app/app.js
+++ app/app.js
@@ -1,12 +1,51 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var myApp = angular.module("myCV", []);
+
+myApp.controller("getAboutMe", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/Info.json').success(function(data) {
+    $scope.result = data;
+  });
 }]);
+
+myApp.controller("getSummary", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/summary.json').success(function(data) {
+    $scope.result = data;
+  });
+}]);
+
+myApp.controller("getExperience", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/experience.json').success(function(data) {
+    $scope.result = data;
+  });
+}]);
+
+myApp.controller("getProject", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/project.json').success(function(data) {
+    $scope.result = data;
+  });
+}]);
+
+myApp.controller("getSkill", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/skill.json').success(function(data) {
+    $scope.result = data;
+  });
+}]);
+
+myApp.controller("getEducation", ['$scope', '$http', function($scope, $http){
+  $http.get('Profile/education.json').success(function(data) {
+    $scope.result = data;
+  });
+}]);
+
+myApp.controller("navController", function ($scope, $location) {
+  $scope.isCollapsed = true;
+
+  $scope.$on('$routeChangeSuccess', function () {
+    $scope.isCollapsed = true;
+    alert("huhuuhu");
+  });
+});
+
+
